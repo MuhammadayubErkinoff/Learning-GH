e50a6c4d6a4fd2f0ebee17bad849c7b26cb54395
Muhammadayub Erkinoff <muhammadayub2006@gmail.com>
2022-01-01 19:45:02 +0500

    Initial Commit

---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by Muhammadayubxon on 01/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
